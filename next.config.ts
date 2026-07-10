55a36e036c1471cbfc4e2b557d720ef0809e2bc0
PanduAshidiq <pandudwi120@gmail.com>
2026-07-10 20:41:01 +0700

    refactor: remove standalone output configuration and update deployment workflow to standard build and containerized dependency management

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: "standalone",
     cacheComponents: true,
-    serverExternalPackages: ["@prisma/client", "cloudinary", "pg", "@prisma/adapter-pg"],
     images: {
         qualities: [25, 50, 75, 100],
         remotePatterns: [
