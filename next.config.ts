3a138f3b9d8c75808c724fed178b379ba0f4c0b0
PanduAshidiq <pandudwi120@gmail.com>
2026-07-10 19:48:52 +0700

    fix: adjust deployment sync flags and include Prisma Postgres dependencies in standalone build

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: "standalone",
     cacheComponents: true,
-    serverExternalPackages: ["@prisma/client", "cloudinary"],
+    serverExternalPackages: ["@prisma/client", "cloudinary", "pg", "@prisma/adapter-pg"],
     images: {
         qualities: [25, 50, 75, 100],
         remotePatterns: [
